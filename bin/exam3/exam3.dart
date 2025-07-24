268365849a0e0e65f1c9bc63ca84d32ae9211226
Olim <sayyid.olim12@gmail.com>
2025-07-25 01:07:36 +0700

    Success fix todo 1, 2, 3 exam 3

--- bin/exam3/exam3.dart
+++ bin/exam3/exam3.dart
@@ -1,7 +1,7 @@
 Set uniqueElement(List<int> myList) {
   // TODO 1
 
-  return {};
+  return myList.toSet();
 
   // End of TODO 1
 }
@@ -9,7 +9,13 @@ Set uniqueElement(List<int> myList) {
 Map<String, String> buildFutsalPlayersMap() {
   // TODO 2
 
-  return {};
+  return {
+    'Goalkeeper': 'Andri',
+    'Anchor': 'Irfan',
+    'Pivot': 'Fikri',
+    'Right Flank': 'Aldi',
+    'Left Flank': 'Hafid'
+  };
 
   // End of TODO 2
 }
@@ -18,6 +24,10 @@ Map<String, String> updatePivotPlayer() {
   final futsalPlayers = buildFutsalPlayersMap();
 
   // TODO 3
+
+  futsalPlayers['Pivot'] = 'Fajar'; //get key for update the value
+  //other method i create, if want use .update
+  // futsalPlayers.update('Pivot', (value) => 'Fajar');
 
   // End of TODO 3
 
