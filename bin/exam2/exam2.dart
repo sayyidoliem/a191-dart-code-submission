ec693e69cf7fa801efa6db539c6546b599d4ad9b
Olim <sayyid.olim12@gmail.com>
2025-07-28 23:11:01 +0700

    Success fix todo 1, 2, 3 exam 2

--- bin/exam2/exam2.dart
+++ bin/exam2/exam2.dart
@@ -1,7 +1,7 @@
 dynamic oddOrEven(int number) {
   // TODO 1
 
-  return null;
+  return number % 2 == 0 ? 'Genap' : 'Ganjil'; // more readable than if-else
 
   // End of TODO 1
 }
@@ -10,6 +10,15 @@ dynamic createListOneToX(int x) {
   final List<int> list = [];
 
   // TODO 2
+
+  if (x < 1) {
+    return list; // if x less than 1, return 0 or []
+  } else {
+    for (int i = 1; i <= x; i++) {
+      // for counting total value on list
+      list.add(i);
+    }
+  }
 
   // End of TODO 2
 
@@ -20,6 +29,11 @@ String getStars(int n) {
   var result = '';
 
   // TODO 3
+
+  for (int i = n; i >= 1; i--) {
+    // for make the star from large to small and inverted triangle
+    result += '*' * i + '\n'; // add\n to add new line
+  }
 
   // End of TODO 3
 
